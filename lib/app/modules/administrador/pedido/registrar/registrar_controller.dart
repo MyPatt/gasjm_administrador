7f2021a92314ea3da0104f3be90aed0577b85906
otrospt@gmail.com <otrospt@gmail.com>
2023-01-15 14:28:45 -0500

    Modulo pedido registrar 1.1

--- lib/app/modules/administrador/pedido/registrar/registrar_controller.dart
+++ lib/app/modules/administrador/pedido/registrar/registrar_controller.dart
@@ -1,26 +1,20 @@
-import 'dart:io';
-
+ 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gasjm/app/core/utils/map_style.dart';
-import 'package:gasjm/app/core/utils/mensajes.dart';
-import 'package:gasjm/app/data/controllers/autenticacion_controller.dart';
+import 'package:gasjm/app/core/utils/mensajes.dart'; 
 import 'package:gasjm/app/data/models/horario_model.dart';
 import 'package:gasjm/app/data/models/pedido_model.dart';
 
-import 'package:gasjm/app/data/models/persona_model.dart';
-import 'package:gasjm/app/data/models/vehiculo_model.dart';
+import 'package:gasjm/app/data/models/persona_model.dart'; 
 import 'package:gasjm/app/data/repository/horario_repository.dart';
 import 'package:gasjm/app/data/repository/pedido_repository.dart';
 import 'package:gasjm/app/data/repository/persona_repository.dart';
-import 'package:gasjm/app/data/repository/producto_repository.dart';
-import 'package:gasjm/app/data/repository/vehiculo_repository.dart';
-import 'package:gasjm/app/routes/app_routes.dart';
+import 'package:gasjm/app/data/repository/producto_repository.dart'; 
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart'; 
 import 'package:get/get.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; 
 
 class RegistrarPedidoController extends GetxController {
  

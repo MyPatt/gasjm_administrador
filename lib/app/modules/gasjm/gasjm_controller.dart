5bebd29f97d8a29ed2c16b30bf6088ec80b5d81d
otrospt@gmail.com <otrospt@gmail.com>
2023-01-19 00:59:46 -0500

    Modulo GasJM informacion  5

--- lib/app/modules/gasjm/gasjm_controller.dart
+++ lib/app/modules/gasjm/gasjm_controller.dart
@@ -1,13 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:gasjm/app/core/utils/mensajes.dart';
+import 'package:gasjm/app/data/models/gasjm_model.dart';
 import 'package:gasjm/app/data/models/horario_model.dart';
+import 'package:gasjm/app/data/models/producto_model.dart';
 import 'package:gasjm/app/data/repository/horario_repository.dart';
 import 'package:get/get.dart';
 
 class GasJMController extends GetxController {
   //Repositorio de horario
-  final _horarioRepository = Get.find<GasJMRepository>();
+  final _gasJMRepository = Get.find<GasJMRepository>();
 
   /* Variables para obtener datos del horario*/
   final RxList<HorarioModel> _lista = <HorarioModel>[].obs;
@@ -18,30 +19,67 @@ class GasJMController extends GetxController {
   // //Mientras se inserta el pedido mostrar circuleprobres se carga si o no
   final actualizandoHorario = RxBool(false);
 
-  //
+  //se envia el modo para cargar la lista de los pedidos el administrador carga de todos
+  //y el repartidor solo los correspondientes y al actualizar se cargan de nuevo la lista
+  //refactorizado el codigo actualizar para usar por el administrador y repartidor
+  // 0 administrador
+  // 1 repartidor
   int modo = Get.arguments;
-  //
+
+  //Variables para el tabarview Informacion
+  GasJm gasJM=GasJm();
+  ProductoModel productoModel=const ProductoModel(nombreProducto: '', precioProducto: 0.0);
+  //aMETODSO PROPIOS
   @override
   void onInit() {
     super.onInit();
     //
-    cargarDatos();
+    //cargarDatos();
+    cargarDatosHorarios();
   }
 
-  Future<void> cargarDatos() async {
-//
+  caargarDatos()   {
+    cargarInformacionDistribuidora();
+    cargarInformacionProducto();
+    
+  }
+
+//Obtner informacion de la distribuidora y del producto desde firestore
+  Future<void> cargarInformacionDistribuidora() async {
     try {
-      _lista.value = await _horarioRepository.getListaHorarios();
+      gasJM = await _gasJMRepository.getInformacionDistribuidora();
     } catch (e) {
-      //
+      Exception(
+          'Ha ocurrido un error, por favor inténtelo de nuevo más tarde.');
     }
   }
 
+//Obtner informacion de la distribuidora y del producto desde firestore
+  Future<void> cargarInformacionProducto() async {
+    try {
+      productoModel = await _gasJMRepository.getProducto();
+    } catch (e) {
+      Exception(
+          'Ha ocurrido un error, por favor inténtelo de nuevo más tarde.');
+    }
+  }
+
+//Obtner datos de los horarios desde firestore
+  Future<void> cargarDatosHorarios() async {
+    try {
+      _lista.value = await _gasJMRepository.getListaHorarios();
+    } catch (e) {
+      Exception(
+          'Ha ocurrido un error, por favor inténtelo de nuevo más tarde.');
+    }
+  }
+
+//En modo admin se puede aditar el horario
   Future<void> actualizarHorario(HorarioModel horario) async {
     try {
       actualizandoHorario.value = true;
       //
-      await _horarioRepository.updateHorario(
+      await _gasJMRepository.updateHorario(
           uidHorario: horario.uidHorario,
           horaApertura: horario.aperturaHorario,
           horaCierre: horario.cierreHorario);
